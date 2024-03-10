1d025ad61a00d12836b961bc9ec10532f7cea027
Shri Ganesh Jha <60005430+TheSGJ@users.noreply.github.com>
2024-03-11 00:20:39 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -107,16 +107,15 @@ const Contact = () => {
                 </div>
               </div>
               <div className="p-2 w-full">
-                <Button
-                  className="pageclip-form__submit"
-                  variant="contained"
-                  fullWidth
-                  color="primary"
+                <button
+                  className="pageclip-form__submit relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800"
                   type="submit"
                   disabled={state.submitting}
                 >
-                  Submit
-                </Button>
+                  <span>
+                    Submit
+                  </span>
+                </button>
               </div>
               <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
                 <p className="text-indigo-500">
